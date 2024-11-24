83bb0d7b534cf8d1d0ed74eb4820b33ebd3e51dd
Charbel Maroun <charbel.maroun@ucalgary.ca>
2024-11-24 16:40:58 -0700

    Added main Folders

---
+++ Backend/src/test/java/ENSF480/uofc/Backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ENSF480.uofc.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
